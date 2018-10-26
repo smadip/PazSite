581a801f26da2c02deea2d21c4ccc86d04089ab0
smadip <smadip@gmail.com>
2018-10-26 09:46:12 +0300

    add change image

---
+++ script.js
@@ -0,0 +1,25 @@
+function onLoadFunction(){
+    setInterval( ChangeImageSrc, 3000);
+}
+
+function ChangeImageSrc(){
+    
+    var imgSrc1 = "paz_250x110103131473.jpg";
+    var imgSrc2 = "paz_250x110059890678.jpg";
+    var imgSrc3 = "250X110668568400.jpg";
+
+    var currentImgSrc = document.getElementById("imgChange").src;
+    if(currentImgSrc.includes(imgSrc1))
+    {
+        document.getElementById("imgChange").src ="images/"+ imgSrc2;
+    }
+    else if(currentImgSrc.includes(imgSrc2))
+    {
+        document.getElementById("imgChange").src = "images/"+ imgSrc3;
+        
+    }
+    else
+    {
+        document.getElementById("imgChange").src = "images/"+ imgSrc1
+    }
+}
